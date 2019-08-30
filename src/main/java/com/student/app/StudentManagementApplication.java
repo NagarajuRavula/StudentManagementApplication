53e38a2d3fd794df2fc9f2b671039277a0ba73aa
NagarajuRavula <nagaraju.ravula@imaginea.com>
2019-08-30 17:11:25 +0530

    Added the configuration to run in tomcat

--- src/main/java/com/student/app/StudentManagementApplication.java
+++ src/main/java/com/student/app/StudentManagementApplication.java
@@ -3,14 +3,21 @@ package com.student.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ImportResource;
 
 
 @ImportResource("classpath:spconfig.xml")
 @SpringBootApplication
-public class StudentManagementApplication {
+public class StudentManagementApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StudentManagementApplication.class, args);
-	}
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(StudentManagementApplication.class);
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(StudentManagementApplication.class, args);
+  }
 }
